81d810eadffa712138cd27a9395638485a9ef87d
Candela Hernik <johndoe@example.com>
2021-12-29 20:04:50 -0300

    subida a github

---
+++ clases.js
@@ -0,0 +1,10 @@
+class Consulta{
+    constructor(nombre, apellido, direccion, email, consulta) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.direccion = direccion;
+        this.email = email; 
+        this.consulta = consulta;
+    }
+}
+
